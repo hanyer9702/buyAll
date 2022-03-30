ec527b7318c61eb9538774f1207f0ec10e06054b
owner <hanyer9702@naver.com>
2022-03-30 14:06:34 +0900

    로그인 Ui

---
+++ src/main/java/com/buy/all/modules/member/MemberController.java
@@ -0,0 +1,39 @@
+package com.buy.all.modules.member;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class MemberController {
+	
+//	참고영역
+	@RequestMapping(value = "/user/include/head")
+	public String footer(Model model) throws Exception {
+				
+		return "/user/include/head";
+	}
+	@RequestMapping(value = "/user/include/bottom")
+	public String bottom(Model model) throws Exception {
+		
+		return "/user/include/bottom";
+	}
+	
+//	로그인
+	@RequestMapping(value = "/user/login")
+	public String login(Model model) throws Exception {
+		
+		return "/user/login/login";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/user/include/bottom.jsp
@@ -0,0 +1,3 @@
+	<script src="/resources/common/_bootstrap/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
